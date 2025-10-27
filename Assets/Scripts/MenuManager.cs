9edb39b385f384f48fe74ed0eceef76a77190be5
SKM2027 <gamergalhabit@gmail.com>
2025-10-27 17:34:57 -0400

    Update MenuManager.cs

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -28,21 +28,8 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene(1);
         //not sure what scene should go next
         Debug.Log("Play!!");
-    }*/
-    public void contiune()//makes sure your update scene is number 2
-    {
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
-        SceneManager.LoadScene(2);
-        //not sure what scene should go next
-        Debug.Log("Play!!");
     }
-    public void newGame()//makes sure your update scene is number 1
-    {
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
-        SceneManager.LoadScene(1);
-        //not sure what scene should go next
-        Debug.Log("Play!!");
-    }
+    
     public void OpenOptions()
     {
      
